30326e3cd3f40c4eb0d724d7ecb382d8e652fd40
TaggartCook <taggart.cook2ucas-edu.net>
2024-11-15 11:49:17 -0700

    blahhh

--- dessert.py
+++ dessert.py
@@ -1,6 +1,14 @@
-class DessertItem:
+from abc import ABC, abstractmethod
+class DessertItem(ABC):
     def __init__(self,name):
         self.name = name
+        self.tax_percent = 7.25
+
+    @abstractmethod
+
+    def calculate_cost(self):
+        pass
+
 
 class Candy(DessertItem):
     def __init__(self,name,candy_weight,price_per_pound):
@@ -8,23 +16,43 @@ class Candy(DessertItem):
         self.candy_weight = candy_weight
         self.price_per_pound = price_per_pound
 
+    def calculate_cost(self):
+        cost = self.candy_weight * self.price_per_pound
+        return cost
+
+    def calculate_tax(self):
+        tax = self.calculate_cost * (self.tax_percent)
+        return tax
+
 class Cookie(DessertItem):
     def __init__(self,name,cookie_quantity,price_per_dozen):
         super().__init__(name)
         self.cookie_quantity = cookie_quantity
         self.price_per_dozen = price_per_dozen
 
+    def calculate_cost(self):
+        cost = self.cookie_quantity * self.price_per_dozen
+        return cost
+
 class IceCream(DessertItem):
     def __init__(self,name,scoop_count,price_per_scoop):
         super().__init__(name)
         self.scoop_count = scoop_count
         self.price_per_scoop = price_per_scoop
+   
+    def calculate_cost(self):
+        cost = self.scoop_count * self.price_per_scoop
+        return cost
 
 class Sundae(IceCream):
     def __init__(self,name,scoop_count,price_per_scoop,topping_name,topping_price):
         super().__init__(name,scoop_count,price_per_scoop)
         self.topping_name = topping_name
         self.topping_price = topping_price
+    
+    def calculate_cost(self):
+        cost = (self.scoop_count * self.price_per_scoop)+(self.topping_price)
+        return cost
 
 class Order():
     def __init__(self):
@@ -35,6 +63,18 @@ class Order():
 
     def add(self,added_item):
         self.order.append(added_item)    
+
+    def order_cost(self):
+        total_cost = 0.00
+        for i in self.order:
+            total_cost += i.cost()
+        return total_cost
+    
+    def order_tax(self):
+        total_tax = 0.00
+        for i in self.order:
+            total_tax += i.tax()
+        return round(total_tax)
 
 def main():
     order1 = Order()
@@ -47,7 +87,14 @@ def main():
         
     for item in order1.order:
         print(item.name)
+    #data: list[list[str,int,float]]
+    data = []
+    for i in order1.order:
+        data.append([i.name,i.calculate_cost(),round(i.calculate_tax())])
+    data.append(["Order Subtotals",order1.order_cost,order1.order_tax])
+    data.append(["Order Total", order1.order_cost() + order1.order_tax()])
+
 
     print("Total number of items in order:", len(order1))
-    
+
 main()
